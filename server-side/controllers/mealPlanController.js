987692e28b879ec92390b05abf744dbe192aacba
Fatjon Gashi <fg47842@ubt-uni.net>
2024-05-22 04:18:29 +0200

    added api to get the mealPlan of the user for current day of the week

--- server-side/controllers/mealPlanController.js
+++ server-side/controllers/mealPlanController.js
@@ -1,3 +1,4 @@
+const DietAssessment = require("../models/DietAssessmentSchema");
 const Diet = require("../models/DietTypeSchema");
 const MealPlan = require("../models/MealPlanSchema");
 
@@ -41,6 +42,51 @@ const mealPlan = {
             res.status(500).send("Internal Server Error!");
         }
     },
+
+    getMealPlanByUser: async (req, res) => {
+        try {
+            const user = req.user;
+    
+            // Find the user's diet assessment and ensure it is awaited and properly handled
+            const userDiet = await DietAssessment.findOne({ user: user._id }).sort({ date: -1 });
+    
+            if (!userDiet) {
+                return res.status(404).send("You do not have a diet yet!");
+            }
+    
+            // Find the diet associated with the user's diet assessment
+            const diet = await Diet.findOne({ type: userDiet.dietType });
+    
+            if (!diet) {
+                return res.status(404).send("Diet not found!");
+            }
+    
+            // Determine today's day of the week (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
+            const today = new Date().getDay();
+            const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+            const todayString = daysOfWeek[today];
+    
+            // Find the meal plan associated with the found diet and today's day
+            const mealPlan = await MealPlan.findOne({ dietType: diet._id, 'days.day': todayString });
+    
+            if (!mealPlan) {
+                return res.status(404).send("Meal plan not found for today!");
+            }
+    
+            // Extract the meals for today
+            const todayMeals = mealPlan.days.find(day => day.day === todayString);
+    
+            if (!todayMeals || todayMeals.meals.length === 0) {
+                return res.status(404).send("No meals found for today!");
+            }
+    
+            res.json(todayMeals.meals);
+    
+        } catch (error) {
+            console.log(error.message);
+            res.status(500).send("Internal Server Error!");
+        }
+    }
 }
 
 module.exports = mealPlan
--- server-side/routes/mealPlanRoute.js
+++ server-side/routes/mealPlanRoute.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mealPlan = require("../controllers/mealPlanController");
+const passport = require("passport");
 
 const mealPlanRoutes = express.Router();
 
 mealPlanRoutes.post("/", mealPlan.addMealPlan);
+mealPlanRoutes.get("/getMealPlanByUser", passport.authenticate('jwt', { session: false }), mealPlan.getMealPlanByUser);
 mealPlanRoutes.get("/:id", mealPlan.getMealPlan);
 
 module.exports = mealPlanRoutes;

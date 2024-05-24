1d2d61a2fa07b3e8b2131d3c038c998b24f9237d
Fatjon Gashi <fg47842@ubt-uni.net>
2024-05-24 06:01:02 +0200

    added apis for data assessment

--- server-side/controllers/DataAssessmentController.js
+++ server-side/controllers/DataAssessmentController.js
@@ -12,6 +12,55 @@ const DataAssessment = {
         } catch (error) {
             res.json("Internal Server Error!");
         }
+    },
+
+    addDataAssessmentFromUser: async (req,res) => {
+        const { answers, dietType } = req.body;
+
+        try {
+            const user = req.user;
+
+            const lastDiet = await DietAssessment.findOne({ user: user._id }).sort({ date: -1 });
+
+            if (lastDiet) {
+                const lastDietDate = new Date(lastDiet.date);
+                const oneMonthAgo = new Date();
+                oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1);
+
+                if (lastDietDate >= oneMonthAgo) {
+                    return res.status(400).json({ message: 'You can only add a new DataAssessment if the last one is over a month old!' });
+                }
+            }
+
+            const dataDiet = new DietAssessment({
+                user: user._id,
+                answers,
+                dietType
+            });
+
+            await dataDiet.save();
+            return res.status(201).json({ message: 'DataAssessment added successfully' });
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({ message: 'Server error' });
+        }
+    },
+
+    getTwoLastAssessments: async (req,res) => {
+        try {
+            const user = req.user;
+    
+            const getDietAssessment = await DietAssessment.find({user: user._id}).sort({date: -1}).limit(2);
+    
+            if(getDietAssessment.length !== 2){
+                return res.send(400).json({message: "You don't have enough data yet!"});
+            }
+
+            res.json(getDietAssessment);
+            
+        } catch (error) {
+            return res.status(500).json({ message: '"Internal Server Error!"' });
+        }
     }
 }
 
--- server-side/routes/dietAssessmentRoute.js
+++ server-side/routes/dietAssessmentRoute.js
@@ -4,5 +4,7 @@ const dietAssessmentRoute = express.Router();
 const passport = require('passport')
 
 dietAssessmentRoute.get("/", passport.authenticate('jwt', { session: false }), DataAssessment.getDataAssessmentFromUser);
+dietAssessmentRoute.post("/addDietAssessment", passport.authenticate('jwt', { session: false }), DataAssessment.addDataAssessmentFromUser);
+dietAssessmentRoute.get("/getTwoLastAssessment", passport.authenticate('jwt', { session: false }), DataAssessment.getTwoLastAssessments);
 
 module.exports = dietAssessmentRoute;
